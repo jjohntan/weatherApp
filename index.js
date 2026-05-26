704051a73aabbed21d3748989566594d27a45b2f
tanjekjohn@gmail.com <tanjekjohn@gmail.com>
2026-05-26 17:36:49 +0800

    feat: add weather form event handler and prevent default from submitting

---
+++ index.js
@@ -0,0 +1,9 @@
+// get the first element from the document
+const weatherForm = document.querySelector('.weatherForm');
+
+weatherForm.addEventListener('submit', event => {
+    // prevent submitting behavior
+    event.preventDefault();
+
+
+});
