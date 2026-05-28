224732fad24af01c92d810049b8c6daa6e389ab5
tanjekjohn@gmail.com <tanjekjohn@gmail.com>
2026-05-28 11:51:32 +0800

    feat: add weather display function - error handling for empty api - extract data from api

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ weatherForm.addEventListener('submit', async event => {
     }
     else{
         const weatherData = await getWeatherData(city);
+        displayWeatherInfo(weatherData);
     }
 });
 
 async function getWeatherData(city){
     const apiUrl 
-    = `https://api.data.gov.my/weather/forecast?contains=${city}@location__location_id`;
+    = `https://api.data.gov.my/weather/forecast?contains=${city}@location__location_name`;
 
     const response = await fetch(apiUrl);
 
@@ -28,5 +29,36 @@ async function getWeatherData(city){
         throw new Error('Could not fetch weather data');
     }
 
-    console.log(response);
+    return response.json();
+}
+
+function displayWeatherInfo(data){
+    
+    if (!data || data.length === 0){
+        console.log(`Errorrrrr data not found`);
+        return;
+    }
+
+    //get first data from array
+    const weatherRecord = data[0];
+
+    // extract data like city name and forecast
+    const cityName = weatherRecord.location.location_name;
+    const morningForecast = weatherRecord.morning_forecast;
+    const afternoonForecast = weatherRecord.afternoon_forecast;
+    const nightForecast = weatherRecord.night_forecast;
+    const summaryForecast = weatherRecord.summary_forecast;
+    const summaryWhen = weatherRecord.summary_when;
+    const minTemp = weatherRecord.min_temp;
+    const maxTmep = weatherRecord.max_temp;
+
+
+    console.log(`City name: ${cityName}`);
+    console.log(`Morning forecast: ${morningForecast}`);
+    console.log(`AfternoonForecast forecast: ${afternoonForecast}`);
+    console.log(`Night forecast: ${nightForecast}`);
+    console.log(`summary forecast: ${summaryForecast}`);
+    console.log(`Summary when: ${summaryWhen}`);
+    console.log(`Min temperature: ${minTemp}`);
+    console.log(`Max temperature: ${maxTmep}`);
 }
